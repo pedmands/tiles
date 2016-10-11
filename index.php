3b648a8593f54cb683f4e4f3392269f78dee7eb2
Preston Edmands <pedmands@gmail.com>
2016-10-11 19:17:01 -0400

    Added conditional statement to Loop if no posts are found

--- index.php
+++ index.php
@@ -5,19 +5,21 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
    <div id="content" class="site-content">
-	
-
-<?php 
-	if ( have_posts() ) : 
+<?php
+	if ( have_posts() ) {
+		tiles_content_nav( 'nav-above' );
 		// Start the Loop
-		while ( have_posts() ) : the_post(); 
+		while ( have_posts() ) : the_post();
         	get_template_part( 'content-templates/content', get_post_format() );
-	    endwhile; 
-   	endif; 
+	    endwhile;
+	    tiles_content_nav( 'nav-below' );
+	}else{
+		get_template_part('no-results','index');
+	}
            ?>
    </div><!-- #content .site-content -->
 </div>
 <!-- #primary .content-area -->
-          
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
