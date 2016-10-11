57bb99afe90b1bd44444eee9b47ac2fd75949b78
Preston Edmands <pedmands@gmail.com>
2016-10-11 18:23:58 -0400

    enqueued styles and scripts.

--- functions.php
+++ functions.php
@@ -50,3 +50,19 @@ if ( ! function_exists('tiles_setup')) :
 	} // tiles_setup
 endif; 
 add_action('after_setup_theme', 'tiles_setup');
+
+// Enqueue scripts and styles
+function tiles_scripts() {
+	wp_enqueue_style('style', get_stylesheet_uri());
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
+
+	wp_enqueue_script('navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20161011', true);
+
+	if (is_singular() && wp_attachment_image()) {
+		wp_enqueue_script('keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array('jquery'), '20161011' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'tiles_scripts');
