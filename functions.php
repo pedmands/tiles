d794f061bf5a202ad23905850d095e9775e7bf8c
Preston Edmands <pedmands@gmail.com>
2016-10-11 15:53:32 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Tiles functions and definitions
+ *
+ * @package Tiles
+ * @since Tiles 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ *
+ * @since Shape 1.0
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 654; /* pixels */
+
