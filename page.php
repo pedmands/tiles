a969d8109b7f20f3314a8a5cb434e4ff0e5fd8e2
Preston Edmands <pedmands@gmail.com>
2016-10-12 20:50:52 -0400

    Added Page.php

--- page.php
+++ page.php
@@ -1,1 +1,23 @@
-page.php
+<?php /**
+ * The template for displaying all Pages.
+ * @package Tiles
+ * @since Tiles 1.0
+ */
+get_header(); ?>
+ 
+        <div id="primary" class="content-area">
+            <div id="content" class="site-content" role="main">
+ 
+                <?php while ( have_posts() ) : the_post(); ?>
+ 
+                    <?php get_template_part( 'content', 'page' ); ?>
+ 
+                    <?php comments_template( '', true ); ?>
+ 
+                <?php endwhile; // end of the loop. ?>
+ 
+            </div><!-- #content .site-content -->
+        </div><!-- #primary .content-area -->
+ 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
