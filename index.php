0e90648b64b60418d42ad6f81afbafb2c64deace
Preston Edmands <pedmands@gmail.com>
2016-10-12 20:43:34 -0400

    Better commenting means a  better EU Experience

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ get_header(); ?>
 		tiles_content_nav( 'nav-above' );
 		// Start the Loop
 		while ( have_posts() ) : the_post();
+			/* Include the Post-Format-specific template for the content.
+			 * If you want to override this in a child theme, then include a file in template-parts/
+			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
         	get_template_part( 'template-parts/content', get_post_format() );
 	    endwhile;
 	    tiles_content_nav( 'nav-below' );
