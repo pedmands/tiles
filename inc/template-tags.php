6579ab1b48899bfc12699d43afa48c58f1f992d4
Preston Edmands <pedmands@gmail.com>
2016-10-12 20:12:21 -0400

    Added function tiles_comment for displaying  ping/trackbacks and comments

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -102,3 +102,54 @@ function tiles_content_nav( $nav_id ) {
     <?php
 }
 endif; // tiles_content_nav
+
+if ( ! function_exists( 'tiles_comment' ) ) :
+// Template for comments and pingbacks.
+// Used as a callback by wp_list_comments() for displaying the comments.
+function tiles_comment( $comment, $args, $depth ) {
+    $GLOBALS['comment'] = $comment;
+    switch ( $comment->comment_type ) :
+        case 'pingback' :
+        case 'trackback' :
+    ?>
+    <li class="post pingback">
+        <p><?php _e( 'Pingback:', 'tiles' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'tiles' ), ' ' ); ?></p>
+    <?php
+            break;
+        default :
+    ?>
+    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+        <article id="comment-<?php comment_ID(); ?>" class="comment">
+            <footer>
+                <div class="comment-author vcard">
+                    <?php echo get_avatar( $comment, 50 ); ?>
+                    <?php printf( __( '%s <span class="says">says:</span>', 'tiles' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+                </div><!-- .comment-author .vcard -->
+                <?php if ( $comment->comment_approved == '0' ) : ?>
+                    <em><?php _e( 'Your comment is awaiting moderation.', 'tiles' ); ?></em>
+                    <br />
+                <?php endif; ?>
+ 
+                <div class="comment-meta commentmetadata">
+                    <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
+                    <?php
+                        /* translators: 1: date, 2: time */
+                        printf( __( '%1$s at %2$s', 'tiles' ), get_comment_date(), get_comment_time() ); ?>
+                    </time></a>
+                    <?php edit_comment_link( __( '(Edit)', 'tiles' ), ' ' );
+                    ?>
+                </div><!-- .comment-meta .commentmetadata -->
+            </footer>
+ 
+            <div class="comment-content"><?php comment_text(); ?></div>
+ 
+            <div class="reply">
+                <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+            </div><!-- .reply -->
+        </article><!-- #comment-## -->
+ 
+    <?php
+            break;
+    endswitch;
+}
+endif; // ends check for tiles_comment()
