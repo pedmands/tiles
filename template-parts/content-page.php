1c938bb0930f330864664c706353a7a42b4e3928
Preston Edmands <pedmands@gmail.com>
2016-10-12 20:51:48 -0400

    Page template-part structured.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,1 +1,18 @@
-content-page.php
+<?php /**
+ * The template used for displaying page content in page.php
+ * @package Tiles
+ * @since Tiles 1.0
+ */
+?>
+ 
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header><!-- .entry-header -->
+ 
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'tiles' ), 'after' => '</div>' ) ); ?>
+        <?php edit_post_link( __( 'Edit', 'tiles' ), '<span class="edit-link">', '</span>' ); ?>
+    </div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
