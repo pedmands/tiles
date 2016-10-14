6ccebd018f4ac9db5a617c07fcd8245a21adf77f
Preston Edmands <pedmands@gmail.com>
2016-10-14 18:17:32 -0400

    Styled and moved screen reader link

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tiles' ); ?></a>
+	
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
@@ -39,7 +39,7 @@
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
-
+<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tiles' ); ?></a>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tiles' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
